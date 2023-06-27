b606ee813edda96217c21a54217fbcf7ed31c148
Marko Janosevic <markojanosevicsm7@gmail.com>
2023-06-27 20:20:03 +0200

    #9 #10 Implemented shared users overview and share permission revoke

--- src/app/modules/home/share.service.ts
+++ src/app/modules/home/share.service.ts
@@ -17,6 +17,15 @@ export class ShareService {
           contentType: contentType,
           emails: emails
       }
-      return this.http.post(domain + "share", shareInfo)
+      return this.http.post(domain + "shareWithUser", shareInfo)
   }
+
+  getSharedUsers(ownerEmail: string, contentId: string): Observable<string[]> {
+      return this.http.get<string[]>(domain + "getSharedUsers/" + ownerEmail + "/" + contentId);
+  }
+
+  removeSharePermissions(ownerEmail: string, viewerEmail: string, contentId: string): Observable<any> {
+      return this.http.delete(domain + "removeSharePermissions/" + ownerEmail + "/" + viewerEmail + "/" + contentId)
+  }
+
 }
